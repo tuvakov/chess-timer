3999f91e1e36d7e48d592f42c078fa6b8134532a
Jemshit Tuvakov <jemshit.tuvakov@gmail.com>
2022-12-19 23:20:55 +0900

    Setup basic ui layout.

--- app/src/main/java/com/tuvakov/chess/timer/MainActivity.kt
+++ app/src/main/java/com/tuvakov/chess/timer/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.tuvakov.chess.timer
 
 import android.os.Bundle
+import android.text.format.Time
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.tuvakov.chess.timer.ui.theme.ChessTimerTheme
 
@@ -17,22 +20,9 @@ class MainActivity : ComponentActivity() {
             ChessTimerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    TimerScreen(modifier = Modifier.fillMaxSize())
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChessTimerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/tuvakov/chess/timer/TimerScreen.kt
@@ -0,0 +1,98 @@
+package com.tuvakov.chess.timer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material.icons.rounded.StopCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+// NOTE: Maybe one tone gradient that fades towards the middle would be great?
+// NOTE: Maybe sure it's fullscreen.
+
+@Composable
+fun TimerScreen(
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            TimerHalf(
+                modifier = Modifier
+                    .weight(0.5f)
+                    .background(color = Color.Magenta.copy(alpha = 0.5f))
+                    .rotate(180f),
+                time = "20:00",
+                onClick = {}
+            )
+            TimerHalf(
+                modifier = Modifier
+                    .weight(0.5f)
+                    .background(color = Color.Red.copy(alpha = 0.5f)),
+                time = "10:59",
+                onClick = {}
+            )
+        }
+
+        Buttons(onClick = {})
+    }
+}
+
+@Composable
+private fun TimerHalf(
+    modifier: Modifier = Modifier,
+    time: String,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = time, style = MaterialTheme.typography.h3)
+    }
+}
+
+@Composable
+private fun Buttons(
+    modifier: Modifier = Modifier,
+    onClick: (ButtonAction) -> Unit
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(
+            onClick = { /*TODO*/ }) {
+            Icon(
+                modifier = Modifier.size(70.dp),
+                imageVector = Icons.Rounded.PlayArrow,
+                contentDescription = "Play Button"
+            )
+        }
+        Spacer(modifier = modifier.size(12.dp))
+        IconButton(
+            onClick = { /*TODO*/ }) {
+            Icon(
+                modifier = Modifier.size(50.dp),
+                imageVector = Icons.Rounded.StopCircle,
+                contentDescription = "Stop Button"
+            )
+        }
+        Spacer(modifier = modifier.size(12.dp))
+    }
+}
+
+private enum class ButtonAction {
+    START, PAUSE, RESUME, STOP
+}
